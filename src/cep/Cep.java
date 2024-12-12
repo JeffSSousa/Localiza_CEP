5a9a3660c4c978a871a497b49f611046e7ad177b
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2024-12-12 16:49:37 -0300

    Adicionado caixa de dialogo caso o CEP não seja inserido

--- src/cep/Cep.java
+++ src/cep/Cep.java
@@ -11,11 +11,14 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import Atxy2k.CustomTextField.RestrictedTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Cep extends JFrame {
 
@@ -56,60 +59,72 @@ public class Cep extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblCep = new JLabel("CEP");
 		lblCep.setBounds(23, 25, 52, 21);
 		contentPane.add(lblCep);
-		
+
 		txtCep = new JTextField();
 		txtCep.setBounds(54, 25, 139, 20);
 		contentPane.add(txtCep);
 		txtCep.setColumns(10);
-		
+
 		JLabel lblEndereco = new JLabel("Endereço");
 		lblEndereco.setBounds(23, 83, 63, 21);
 		contentPane.add(lblEndereco);
-		
+
 		txtEndereco = new JTextField();
 		txtEndereco.setBounds(96, 83, 326, 20);
 		contentPane.add(txtEndereco);
 		txtEndereco.setColumns(10);
-		
+
 		JLabel lblBairro = new JLabel("Bairro");
 		lblBairro.setBounds(23, 131, 46, 14);
 		contentPane.add(lblBairro);
-		
+
 		txtBairro = new JTextField();
 		txtBairro.setColumns(10);
 		txtBairro.setBounds(96, 128, 326, 20);
 		contentPane.add(txtBairro);
-		
+
 		JLabel lblCidade = new JLabel("Cidade");
 		lblCidade.setBounds(23, 171, 46, 14);
 		contentPane.add(lblCidade);
-		
+
 		txtCidade = new JTextField();
 		txtCidade.setColumns(10);
 		txtCidade.setBounds(96, 168, 200, 20);
 		contentPane.add(txtCidade);
-		
+
 		JLabel lblUf = new JLabel("UF");
 		lblUf.setBounds(354, 171, 23, 14);
 		contentPane.add(lblUf);
-		
+
 		JComboBox cboUf = new JComboBox();
-		cboUf.setModel(new DefaultComboBoxModel(new String[] {"", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}));
+		cboUf.setModel(new DefaultComboBoxModel(
+				new String[] { "", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
+						"PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO" }));
 		cboUf.setBounds(377, 167, 46, 22);
 		contentPane.add(cboUf);
-		
+
 		JButton btnLimpar = new JButton("Limpar");
 		btnLimpar.setBounds(23, 213, 89, 23);
 		contentPane.add(btnLimpar);
-		
+
 		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (txtCep.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Informe o CEP");
+					txtCep.requestFocus();
+				} else {
+					// Buscar CEP
+				}
+			}
+		});
 		btnBuscar.setBounds(234, 24, 89, 23);
 		contentPane.add(btnBuscar);
-		
+
 		JButton btnInfo = new JButton("");
 		btnInfo.setToolTipText("Sobre");
 		btnInfo.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
@@ -118,11 +133,11 @@ public class Cep extends JFrame {
 		btnInfo.setBackground(SystemColor.control);
 		btnInfo.setBounds(370, 11, 52, 48);
 		contentPane.add(btnInfo);
-		
+
 		/* Uso da biblioteca Atxy2k para validação do campo txtCep */
 		RestrictedTextField validar = new RestrictedTextField(txtCep);
 		validar.setOnlyNums(true);
 		validar.setLimit(8);
-		
+
 	}
 }
