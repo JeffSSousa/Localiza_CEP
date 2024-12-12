64b82e48b48b2df124a90219beb0e8b470bf1e79
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2024-12-12 16:28:10 -0300

    Atributos renomeados e Adicionado UFs no projeto

--- src/cep/Cep.java
+++ src/cep/Cep.java
@@ -13,15 +13,16 @@ import javax.swing.JButton;
 import java.awt.SystemColor;
 import java.awt.Cursor;
 import javax.swing.ImageIcon;
+import javax.swing.DefaultComboBoxModel;
 
 public class Cep extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtCep;
+	private JTextField txtEndereco;
+	private JTextField txtBairro;
+	private JTextField txtCidade;
 
 	/**
 	 * Launch the application.
@@ -54,53 +55,54 @@ public class Cep extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("CEP");
-		lblNewLabel.setBounds(23, 25, 52, 21);
-		contentPane.add(lblNewLabel);
+		JLabel lblCep = new JLabel("CEP");
+		lblCep.setBounds(23, 25, 52, 21);
+		contentPane.add(lblCep);
 		
-		textField = new JTextField();
-		textField.setBounds(54, 25, 139, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtCep = new JTextField();
+		txtCep.setBounds(54, 25, 139, 20);
+		contentPane.add(txtCep);
+		txtCep.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("Endereço");
-		lblNewLabel_1.setBounds(23, 83, 63, 21);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblEndereco = new JLabel("Endereço");
+		lblEndereco.setBounds(23, 83, 63, 21);
+		contentPane.add(lblEndereco);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(96, 83, 326, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		txtEndereco = new JTextField();
+		txtEndereco.setBounds(96, 83, 326, 20);
+		contentPane.add(txtEndereco);
+		txtEndereco.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Bairro");
-		lblNewLabel_2.setBounds(23, 131, 46, 14);
-		contentPane.add(lblNewLabel_2);
+		JLabel lblBairro = new JLabel("Bairro");
+		lblBairro.setBounds(23, 131, 46, 14);
+		contentPane.add(lblBairro);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(96, 128, 326, 20);
-		contentPane.add(textField_2);
+		txtBairro = new JTextField();
+		txtBairro.setColumns(10);
+		txtBairro.setBounds(96, 128, 326, 20);
+		contentPane.add(txtBairro);
 		
-		JLabel lblNewLabel_3 = new JLabel("Cidade");
-		lblNewLabel_3.setBounds(23, 171, 46, 14);
-		contentPane.add(lblNewLabel_3);
+		JLabel lblCidade = new JLabel("Cidade");
+		lblCidade.setBounds(23, 171, 46, 14);
+		contentPane.add(lblCidade);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(96, 168, 200, 20);
-		contentPane.add(textField_3);
+		txtCidade = new JTextField();
+		txtCidade.setColumns(10);
+		txtCidade.setBounds(96, 168, 200, 20);
+		contentPane.add(txtCidade);
 		
-		JLabel lblNewLabel_4 = new JLabel("UF");
-		lblNewLabel_4.setBounds(354, 171, 23, 14);
-		contentPane.add(lblNewLabel_4);
+		JLabel lblUf = new JLabel("UF");
+		lblUf.setBounds(354, 171, 23, 14);
+		contentPane.add(lblUf);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(377, 167, 46, 22);
-		contentPane.add(comboBox);
+		JComboBox cboUf = new JComboBox();
+		cboUf.setModel(new DefaultComboBoxModel(new String[] {"", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}));
+		cboUf.setBounds(377, 167, 46, 22);
+		contentPane.add(cboUf);
 		
-		JButton btnNewButton = new JButton("Limpar");
-		btnNewButton.setBounds(23, 213, 89, 23);
-		contentPane.add(btnNewButton);
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setBounds(23, 213, 89, 23);
+		contentPane.add(btnLimpar);
 		
 		JButton btnBuscar = new JButton("Buscar");
 		btnBuscar.setBounds(234, 24, 89, 23);
